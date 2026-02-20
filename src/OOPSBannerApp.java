293ab4d35763eb2ca5e79851dbdb2814573387aa
The-Orgin <dhanuv48@gmail.com>
2026-02-20 17:43:22 +0530

    Coded UC3

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,13 +1,18 @@
 /*
-*OOPSBannerApp UC1
+*OOPSBannerApp UC3
 *
 *@author Dhanush
-*@version 1.0 
+*@version 3.0 
 */
-
 
 public class OOPSBannerApp{
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(String.join(" ","   ***   " , " " , "   ***   " , " " , " ******   " , " " , "   *****     "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " **    ** " , " " , "  **    **   "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " **    ** " , " " , " **          "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " ******   " , " " , "   *****     "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " **       " , " " , "         **  "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " **       " , " " , " **     **   "));
+        System.out.println(String.join(" ","   ***   " , " " , "   ***   " , " " , " **       " , " " , "   *****     "));
     }
 }
